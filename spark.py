b599ac6cf44f50362ee321b105b04587633cee3e
eadarak00 <eadarak2001@gmail.com>
2025-12-21 23:25:00 +0000

    feat: Amelioration du script de spark et de la transformation

--- spark.py
+++ spark.py
@@ -1,11 +1,11 @@
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import avg, col
+from pyspark.sql.functions import avg, col, count
 
-# Ajouter le connecteur Cassandra
+#  Ajouter le connecteur Cassandra
 os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages com.datastax.spark:spark-cassandra-connector_2.13:3.5.0 pyspark-shell"
 
-#  Créer la SparkSession avec configuration Cassandra
+# Créer la SparkSession avec configuration Cassandra
 spark = SparkSession.builder \
     .appName("CassandraProduits") \
     .master("local[*]") \
@@ -23,25 +23,27 @@ df_produits = spark.read \
 print("Données existantes dans produits :")
 df_produits.show(10)
 
-# Analyse 1 : prix moyen par produit
-print("Prix moyen par produit :")
-df_produits.groupBy("nom") \
-    .agg(avg("prix").alias("prix_moyen")) \
-    .orderBy(col("prix_moyen").desc()) \
-    .show()
+# ---------------- ANALYSES ----------------
 
-# Analyse 2 : comparaison des prix par vendeur
+# Analyse 1 : comparaison des prix par vendeur
 print("Prix moyen par vendeur :")
 df_produits.groupBy("vendeur") \
     .agg(avg("prix").alias("prix_moyen")) \
     .orderBy(col("prix_moyen").desc()) \
     .show()
 
-# Analyse 3 : comparaison des prix par catégorie
+# Analyse 2 : comparaison des prix par catégorie
 print("Prix moyen par catégorie :")
 df_produits.groupBy("categorie") \
     .agg(avg("prix").alias("prix_moyen")) \
     .orderBy(col("prix_moyen").desc()) \
+    .show()
+
+# Analyse 3 : nombre de produits par catégorie
+print("Nombre de produits par catégorie :")
+df_produits.groupBy("categorie") \
+    .agg(count("*").alias("nombre_produits")) \
+    .orderBy(col("nombre_produits").desc()) \
     .show()
 
 # Stopper la session Spark
