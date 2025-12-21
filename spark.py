97f7b3c4577d797f5dcbf13a6920be544c5cfde3
eadarak00 <eadarak2001@gmail.com>
2025-12-21 22:30:15 +0000

    feat: changement de la  version  de python et le script spark ajout des requetest

--- spark.py
+++ spark.py
@@ -1,17 +1,48 @@
+import os
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import avg, col
 
-# Création de la session Spark
+# Ajouter le connecteur Cassandra
+os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages com.datastax.spark:spark-cassandra-connector_2.13:3.5.0 pyspark-shell"
+
+#  Créer la SparkSession avec configuration Cassandra
 spark = SparkSession.builder \
-    .appName("Analyse E-commerce") \
+    .appName("CassandraProduits") \
+    .master("local[*]") \
     .config("spark.cassandra.connection.host", "127.0.0.1") \
+    .config("spark.cassandra.connection.port", "9042") \
     .getOrCreate()
 
-# Chargement des données depuis Cassandra
-df = spark.read \
+# Lire la table produits du keyspace ecommerce
+df_produits = spark.read \
     .format("org.apache.spark.sql.cassandra") \
-    .options(table="produits", keyspace="ecommerce") \
+    .options(keyspace="ecommerce", table="produits") \
     .load()
 
-# Vérification du schéma et affichage des 5 premières lignes
-df.printSchema()
-df.show(5)
+# Afficher quelques lignes
+print("Données existantes dans produits :")
+df_produits.show(10)
+
+# Analyse 1 : prix moyen par produit
+print("Prix moyen par produit :")
+df_produits.groupBy("nom") \
+    .agg(avg("prix").alias("prix_moyen")) \
+    .orderBy(col("prix_moyen").desc()) \
+    .show()
+
+# Analyse 2 : comparaison des prix par vendeur
+print("Prix moyen par vendeur :")
+df_produits.groupBy("vendeur") \
+    .agg(avg("prix").alias("prix_moyen")) \
+    .orderBy(col("prix_moyen").desc()) \
+    .show()
+
+# Analyse 3 : comparaison des prix par catégorie
+print("Prix moyen par catégorie :")
+df_produits.groupBy("categorie") \
+    .agg(avg("prix").alias("prix_moyen")) \
+    .orderBy(col("prix_moyen").desc()) \
+    .show()
+
+# Stopper la session Spark
+spark.stop()
